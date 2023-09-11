885aa442e3c0cf10319f0ebe3a4f528a114a2b95
Steven Kitzes <steven.kitzes@gmail.com>
2023-09-11 00:54:43 -0700

    feat: note defs, randomization, custom timer

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # sheet-music-flash-cards
-Two-way flash cards for learning the treble clef for guitar
+
+Two-way flash cards for learning the treble clef for guitar.
+
+## About
+
+The internet and app stores have a fair few flash card apps for learning sheet music notation.  However, these focus on reading a piece of notation and naming the notes displayed.  I have found that it helps a lot to go in both directions, forcing the brain to flex its knowledge to solve the problem of learning the connections between notes and notation from various angles.
---
+++ main.js
@@ -0,0 +1,71 @@
+const timeDefault = 4;
+var timeSeconds = timeDefault;
+var interval = setInterval(intervalFunction, timeSeconds * 1000);
+const touchables = [];
+
+const bar = document.getElementById("bar");
+const timeInput = document.getElementById("time");
+timeInput.value = timeDefault;
+timeInput.addEventListener("change", handleTimeInputChange)
+timeInput.addEventListener("keyup", handleTimeInputChange)
+
+function handleTimeInputChange(evt) {
+  console.log("change!");
+  clearInterval(interval);
+  const newInput = evt.target.value;
+  console.log(newInput);
+  const newVal = parseFloat(newInput);
+  console.log("newVal", newVal)
+  if (isNaN(newVal)) {
+    console.log("got NAN value in timer input");
+    timeInput.value = timeDefault;
+    timeSeconds = timeDefault;
+    interval = setInterval(intervalFunction, timeSeconds * 1000);
+    return;
+  }  
+  timeSeconds = newVal;
+  interval = setInterval(intervalFunction, timeSeconds * 1000);
+}
+
+notes.forEach((note, idx) => {
+  const touchable = document.createElement('div');
+  touchable.classList.add('touchable');
+  touchable.style.top = `${idx * 15}px`;
+  touchable.style.zIndex = 1;
+  // touchable.style.backgroundColor = `rgb(255, 255, ${idx * 10})`;
+  touchable.addEventListener('click', () => {
+    console.log(JSON.stringify(note), "clicked");
+  });
+  bar.appendChild(touchable);
+  touchables.push(touchable);
+});
+
+// every card turn
+function intervalFunction() {
+  const upperChecked = document.getElementById("upper").checked;
+  const middleChecked = document.getElementById("middle").checked;
+  const lowerChecked = document.getElementById("lower").checked;
+
+  const noteOptions = notes.filter(note => {
+    if (upperChecked && note.ledger === "upper") return true;
+    if (middleChecked && note.ledger === "middle") return true;
+    if (lowerChecked && note.ledger === "lower") return true;
+    if (!upperChecked && !middleChecked && !lowerChecked) return true;
+    return false;
+  });
+
+  const idx = Math.floor(Math.random() * noteOptions.length)
+  const currentNote = noteOptions[idx];
+  touchables.forEach((touchable, idx) => {
+    touchable.innerHTML = '';
+    if (currentNote.id == idx) {
+      touchable.style.zIndex = 5;
+      const noteElement = document.createElement('div');
+      noteElement.classList.add('note-element');
+      touchable.appendChild(noteElement);
+    }
+    else touchable.style.zIndex = 1;
+  })
+
+  console.log(JSON.stringify(currentNote));
+};
